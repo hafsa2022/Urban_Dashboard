125a4b3391843e8cd5232ba3c326320c7726a003
Hafsa El Akhdar <h.hafsaelakhdar@gmail.com>
2026-02-12 14:26:13 +0100

    fix path home issue

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/auth" element={<Auth />} />
+            <Route path="/" element={<Auth />} />
             <Route
               path="/dashboard"
               element={
